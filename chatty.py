411b7d0f24922e1b294d064bfc4e1fbc2285d836
Arpit-Pathak14 <92110146+Arpit-Pathak14@users.noreply.github.com>
2022-03-03 21:13:49 +0530

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -1,4 +1,19 @@
-#import modules 
+#required  modules 
 import random
 import datetime
 import platform
+from colorama import Fore, Back, Style
+#Greeting part 
+greets = ["Hello","Hi There!",'Namaskar 🙏','Glad to see you',"Yo ","What's Up ^_^"]
+name="Chatty"
+print(Fore.BLUE+name+":"+random.choice(greets))
+#user input
+user_responses = input("")
+
+#Time 
+current_time = datetime.datetime.now()
+# print(current_time)
+
+#platform 
+plat = platform.platform()
+print(plat)
